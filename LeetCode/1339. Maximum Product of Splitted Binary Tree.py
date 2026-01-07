ee351269d93ec6a6f131a4058e807b84f1cd3bba
MomentsOfSpring <elsmirroad@icloud.com>
2026-01-07 16:29:59 +0300

    day 7

---
+++ LeetCode/1339. Maximum Product of Splitted Binary Tree.py
@@ -0,0 +1,59 @@
+from lc import *
+# ============================================================
+# 1465. Maximum Product of Splitted Binary Tree
+# https://leetcode.com/problems/maximum-product-of-splitted-binary-tree/?envType=daily-question&envId=2026-01-07
+# ============================================================
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxProduct(self, root: Optional[TreeNode]) -> int:
+        total = 0
+        result = float("-inf")
+
+        def dfs(root):
+            nonlocal total, result
+            if not root:
+                return 0
+            current = root.val + dfs(root.left) + dfs(root.right)
+            result = max(result, (total - current) * current)
+            return current
+
+        total = dfs(root)
+        dfs(root)
+        return result % (10**9 + 7)
+
+
+test("""
+Given the root of a binary tree, split the binary tree into two subtrees by removing one edge such that the product of the sums of the subtrees is maximized.
+Return the maximum product of the sums of the two subtrees. Since the answer may be too large, return it modulo 109 + 7.
+Note that you need to maximize the answer before taking the mod and not after taking it.
+ 
+Example 1:
+
+
+Input: root = [1,2,3,4,5,6]
+Output: 110
+Explanation: Remove the red edge and get 2 binary trees with sum 11 and 10. Their product is 110 (11*10)
+
+Example 2:
+
+
+Input: root = [1,null,2,3,4,null,null,5,6]
+Output: 90
+Explanation: Remove the red edge and get 2 binary trees with sum 15 and 6.Their product is 90 (15*6)
+
+ 
+Constraints:
+
+The number of nodes in the tree is in the range [2, 5 * 104].
+1 <= Node.val <= 104
+
+
+""")
+
